282c98e634e67458b11b0d6363082060b21a3f46
Konrad Kühne <konnykuehne@gmail.com>
2017-07-10 16:55:52 +0200

    fiat lux

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ursprung
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject ursprung "0.1.0-SNAPSHOT"
+  :description "platform that provides replikativ peers and logging"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16" :scope "provided"]
+                 [io.replikativ/superv.async "0.2.9"]
+                 [io.replikativ/konserve "0.4.9"]
+                 [io.replikativ/replikativ "0.2.4"]
+                 [com.fzakaria/slf4j-timbre "0.3.5"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.5.2"]]
+  :source-paths ["src/cljs" "src/clj"]
+  :profiles {:dev {:dependencies [[com.fzakaria/slf4j-timbre "0.3.5"]]}})
