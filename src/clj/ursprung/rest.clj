d25a1a235065d0220e360cb1799a208469246270
Konrad Kühne <konnykuehne@gmail.com>
2017-07-11 17:49:30 +0200

    add base routes

---
+++ src/clj/ursprung/rest.clj
@@ -0,0 +1,8 @@
+(ns ursprung.rest
+  (:require [org.httpkit.server :refer [run-server]]
+            [compojure.route :refer [resources not-found]]
+            [compojure.core :refer [defroutes]]))
+
+(defroutes base-routes
+  (resources "/")
+  (not-found "<h1>404. Page not found.</h1>"))
