36c4acda9621977148a0835a26bfef42cac07eaa
Konrad Kühne <konnykuehne@gmail.com>
2017-07-20 20:38:55 +0200

    add some finesse

--- dashboard/src/dashboard/core.cljs
+++ dashboard/src/dashboard/core.cljs
@@ -1,12 +1,13 @@
-(ns dashboard.core
-  (:require ))
+(ns dashboard.core)
 
 (enable-console-print!)
 
 (println "This text is printed from src/dashboard/core.cljs. Go ahead and edit it and see reloading in action.")
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello Ursprung!"}))
+
+(js/alert (-> app-state deref :text))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
